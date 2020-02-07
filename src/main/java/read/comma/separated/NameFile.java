83cd78fc1a05e3e59593e17eb24ccfae9c5e97db
GBB <>
2020-02-06 21:35:48 -0500

    Firt commit

---
+++ src/main/java/read/comma/separated/NameFile.java
@@ -0,0 +1,28 @@
+package read.comma.separated;
+
+public class NameFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i =0; i<10; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Hello world");
+
+		
+		for(int j=0; j<=100; j++) {
+			System.out.println(j);
+		}
+
+	 
+		getNumebr();
+	
+	}
+	
+	
+	public static void getNumebr() {
+		System.out.println("This is The number method");
+	}
+
+}
